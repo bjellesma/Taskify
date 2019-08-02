7d486a929497a69084d5023d3a68b27965e0ae15
bjellesma <william.jellesma@gmail.com>
2019-08-01 22:58:55 -0400

    successfully including bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import VueFlashMessage from 'vue-flash-message'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 require('vue-flash-message/dist/vue-flash-message.min.css');
 
 Vue.config.productionTip = false
 Vue.use(VueFlashMessage)
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
